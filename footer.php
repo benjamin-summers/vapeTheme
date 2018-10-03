d445894047e2bde95f6e0e92d0323b71deecebf9
benjamin-summers <summerstyler2@gmail.com>
2018-10-03 01:31:19 -0400

    customized footer

--- footer.php
+++ footer.php
@@ -11,26 +11,37 @@
 
 ?>
 
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'vape-theme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'vape-theme' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'vape-theme' ), 'vape-theme', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+</div><!-- #content -->
+<footer>
+    <div id="footer-title">
+        <h3>Footer Title</h3>
+    </div>
+    <div id="footer-icons">
+        <ul>
+            <li><i class="fa fa-facebook fa-2x"></i></li>
+            <li><i class="fa fa-facebook fa-2x"></i></li>
+            <li><i class="fa fa-facebook fa-2x"></i></li>
+        </ul>
+    </div>
+    <div id="author-credit">
+        <h6>Site Developed By Atero Marketing Group</h6>
+    </div>
+</footer>
 </div><!-- #page -->
 
-<script src="assets/js/float-panel.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script>
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop()) {
+            $('div.sticky-logo').addClass('site-logo');
+            $('div.sticky-logo ul.logo').css('display', 'inherit');
+            $('div.sticky-logo ul.logo').css('grid-area', 'logo');
+        } else if ($(window).scrollTop(0)) {
+            $('div.sticky-logo').removeClass('site-logo');
+            $('div.sticky-logo ul.logo').css('display', 'none');
+        }
+    })
+</script>
 
 <?php wp_footer(); ?>
 
--- header.php
+++ header.php
@@ -17,31 +17,17 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
     <link href="https://fonts.googleapis.com/css?family=Akronim" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script>
-        $(window).on('scroll', function () {
-            if($(window).scrollTop()) {
-                $('div.sticky-logo').addClass('site-logo');
-                $('h1.logo').css('display', 'inherit');
-                $('h1.logo').css('grid-area', 'logo');
-            } else {
-                $('div.sticky-logo').removeClass('site-logo');
-                $('div.sticky-logo h1.logo').css('display', 'none');
-            }
-        })
-    </script>
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'vape-theme' ); ?></a>
 
 	<header id="masthead" class="site-header">
 		<nav id="site-navigation" class="main-navigation">
             <div class="seperator"></div>
             <div class="sticky-logo">
-                <h1 class="logo">Site Logo</h1>
+                <ul class="logo"><li><img src="<?php bloginfo('template_directory') ?>/assets/images/finished%20logo.png" /></li></ul>
             </div>
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'vape-theme' ); ?></button>
 			<?php
