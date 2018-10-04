d69f6bc3377e833a68454eab244e8e6ba393962e
benjamin-summers <summerstyler2@gmail.com>
2018-10-04 01:26:39 -0400

    added mods page template to theme

---
+++ page-mods.php
@@ -0,0 +1,33 @@
+<?php get_header();
+
+the_post_thumbnail('full', ['class' => 'home-header-image']);
+
+?>
+
+
+<div class="product-icons">
+    <ul>
+        <li>
+            <p>Mods</p>
+        </li>
+        <li>
+            <p>Tanks</p>
+        </li>
+        <li>
+            <p>E-Juice</p>
+        </li>
+    </ul>
+</div>
+
+<div class="seperator-two"></div>
+
+<div class="home-shop-section">
+    <div>
+        <?php get_sidebar(); ?>
+    </div>
+    <div>
+        <?php echo do_shortcode( '[products limit="12" columns="3"]' ); ?>
+    </div>
+</div>
+
+<?php get_footer(); ?>
