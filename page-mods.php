743571048fa5219e5f994b4020ac2e309477abee
benjamin-summers <summerstyler2@gmail.com>
2018-10-04 18:12:46 -0400

    customizing mods template

--- page-mods.php
+++ page-mods.php
@@ -3,9 +3,9 @@
 <div class="mods-feature-section">
     <div class="social-icons">
         <ul>
-            <li><i class="fa fa-facebook fa2x"></i></li>
-            <li><i class="fa fa-facebook fa2x"></i></li>
-            <li><i class="fa fa-facebook fa2x"></i></li>
+            <li><i class="fa fa-facebook-square fa2x"></i></li>
+            <li><i class="fa fa-instagram fa2x"></i></li>
+            <li><i class="fa fa-twitter-square fa2x"></i></li>
         </ul>
     </div>
     <div class="mods-feature-image">
