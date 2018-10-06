1561ded086ae8b81c1564765daf7aff358c7f9eb
benjamin-summers <summerstyler2@gmail.com>
2018-10-05 20:45:15 -0400

    added page for temp control devices

--- page-templates/temp-page.php
+++ page-templates/temp-page.php
@@ -1,1 +1,25 @@
 <?php /* Template Name: mods-temp-page */ ?>
+<?php get_header(); ?>
+
+<div class="mods-sub-image">
+    <img src="http://localhost/wordpress/wp-content/uploads/2018/10/temp-mods-pic.png" alt="tempature control vaporizer">
+</div>
+
+<div class="mods-submenu">
+    <?php 
+    wp_nav_menu( array(
+        'menu'   => 'mods-submenu',
+    ) );
+ ?>
+</div>
+
+<div class="home-shop-section">
+    <div>
+        <?php get_sidebar(); ?>
+    </div>
+    <div>
+        <?php echo do_shortcode( '[products limit="12" columns="3"]' ); ?>
+    </div>
+</div>
+
+<?php get_footer(); ?>
