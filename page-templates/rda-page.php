c77a584b6b58ef54b294f196cad473398b850afa
benjamin-summers <summerstyler2@gmail.com>
2018-10-06 21:42:47 -0400

    created tank template pages and made them responsive

---
+++ page-templates/rda-page.php
@@ -0,0 +1,25 @@
+<?php /* Template Name: rda-tanks-page */ ?>
+<?php get_header(); ?>
+
+<div class="mods-sub-image">
+    <img src="http://localhost/wordpress/wp-content/uploads/2018/10/RDA-TANKS.png" alt="tempature control vaporizer">
+</div>
+
+<div class="mods-submenu">
+    <?php 
+    wp_nav_menu( array(
+        'menu'   => 'tanks-submenu',
+    ) );
+ ?>
+</div>
+
+<div class="home-shop-section">
+    <div>
+        <?php get_sidebar(); ?>
+    </div>
+    <div>
+        <?php echo do_shortcode( '[products limit="12" columns="3"]' ); ?>
+    </div>
+</div>
+
+<?php get_footer(); ?>
